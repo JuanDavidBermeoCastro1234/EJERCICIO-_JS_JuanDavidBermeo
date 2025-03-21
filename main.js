3439e0ac77d1c2336f7d6e3354a5897841407b91
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2025-03-21 12:55:18 -0500

    feat: :sparkles: adding changes in exercise

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ let limites3 = ("[]")
 let limites4 = (expresion)== (`()`);
 let limites5 = (expresion)== (`{}`);
 let limites6 = (expresion)== (`[]`);
+let limites7 = (expresion)== (`[{()}]`);
+let limites8 = (expresion)== (`[()]`);
+let limites9 = (expresion)== (`[{}]`);
+let limites10 = (expresion)== (`{()}`);
 
-
-if (limites4==true || limites5==true || limites6==true  ) {
+if (limites4==true || limites5==true || limites6==true || limites7==true || limites8==true || limites9==true || limites10==true) {
     console.log(`la expresion ${expresion} esta equilibrada`);
 }else{
     console.log (`la expresion ${expresion} no esta equilibrada`);
